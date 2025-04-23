12a44d0853b6d449d0bf7e96a53f9f2f41325d2f
Bart Liefers <bjliefers@gmail.com>
2025-04-23 21:38:26 +0200

    Updating instance query

--- start_server_dev.sh
+++ start_server_dev.sh
@@ -6,5 +6,4 @@ source .env
 set +a
 
 # Start the server
-cd server
-uvicorn main:app --reload --host 0.0.0.0 --port $SERVER_PORT --log-level debug
+uvicorn server.main:app --reload --host 0.0.0.0 --port $SERVER_PORT --log-level debug
