2dbffeffcd19737df379a85cc451f8c7801a661e
Bart Liefers <bjliefers@gmail.com>
2025-05-01 16:19:51 +0200

    config for external data sources, basic css themes

---
+++ client/src/lib/browser/ExternalData.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import { openNewWindow } from "$lib/newWindow";
+    import type { DataRow, DataSource } from "$lib/utils/cached_source_loader";
+    import DataTable from "$lib/utils/DataTable.svelte";
+    interface Props {
+        data: DataSource;
+        name: string;
+    }
+    let { data, name }: Props = $props();
+    
+    function open(tag: string, rows: DataRow[]) {        
+        openNewWindow(DataTable, {data:rows}, tag);
+    }
+</script>
+
+<div>
+    <h4>{name}</h4>
+    <ul>
+        {#each Object.entries(data) as [tag, rows]}
+            <li>
+                <!-- svelte-ignore a11y_click_events_have_key_events -->
+                <!-- svelte-ignore a11y_no_static_element_interactions -->
+                <span onclick={() => open(tag, rows)}>{tag}</span>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+    h4, ul {
+        padding: 0;
+        margin: 0;
+    }
+    h4 {
+        margin-bottom: 0.2em;
+        margin-top: 0.2em;
+        padding-left: 0.5em;
+    }
+    ul {
+        list-style-type: none;
+        margin-left: 1em;
+    }
+    span {
+        cursor: pointer;
+        font-family: monospace;
+
+    }
+    span:hover {
+        text-decoration: underline;
+    }
+</style>
--- client/src/lib/browser/Selection.svelte
+++ client/src/lib/browser/Selection.svelte
@@ -1,63 +1,59 @@
 <script lang="ts">
-	import InstanceComponent from './InstanceComponent.svelte';
-	import { getContext } from 'svelte';
+    import { getContext } from "svelte";
+    import InstanceComponent from "./InstanceComponent.svelte";
 
-	import type { BrowserContext } from './browserContext.svelte';
-	import { page } from '$app/state';
-	import { data } from '$lib/datamodel/model';
+    import { data } from "$lib/datamodel/model";
+    import type { BrowserContext } from "./browserContext.svelte";
 
-	const browserContext = getContext<BrowserContext>('browserContext');
-	const { instances } = data;
+    const browserContext = getContext<BrowserContext>("browserContext");
+    const { instances } = data;
 
-	function clear() {
-		browserContext.selection = [];
-	}
+    function clear() {
+        browserContext.selection = [];
+    }
 
-	function openSelectionTab() {
-		const instances = browserContext.selection.join(',');
-		let suffix_string = `?instances=${instances}`;
-        
-		const creatorid = page.url.searchParams.get('creatorid');
-		if (creatorid) {
-			suffix_string += `&creatorid=${creatorid}`;
-		}
-
-		const url = `${window.location.origin}/view${suffix_string}`;
-		window.open(url, '_blank')?.focus();
-	}
+    function openSelectionTab() {
+        browserContext.openTab(browserContext.selection);
+    }
 </script>
 
 <div>
-	<div class="button-container">
-		<div>
-			{browserContext.selection.length}
-			{browserContext.selection.length != 1 ? 'images' : 'image'} selected
-		</div>
-		<button disabled={browserContext.selection.length === 0} onclick={openSelectionTab}>
-			Open selected images
-		</button>
-		<button disabled={browserContext.selection.length === 0} onclick={clear}>
-			Clear selection
-		</button>
-	</div>
-	<div>
-		{#each browserContext.selection as instanceId (instanceId)}
-			<InstanceComponent instance={instances.get(instanceId)!} />
-		{/each}
-	</div>
+    <div class="button-container">
+        <div>
+            {browserContext.selection.length}
+            {browserContext.selection.length != 1 ? "images" : "image"} selected
+        </div>
+        <button
+            disabled={browserContext.selection.length === 0}
+            onclick={openSelectionTab}
+        >
+            Open selected images
+        </button>
+        <button
+            disabled={browserContext.selection.length === 0}
+            onclick={clear}
+        >
+            Clear selection
+        </button>
+    </div>
+    <div>
+        {#each browserContext.selection as instanceId (instanceId)}
+            <InstanceComponent instance={instances.get(instanceId)!} />
+        {/each}
+    </div>
 </div>
 
 <style>
-	div {
-		display: flex;
-		background-color: black;
-		color: white;
-	}
-	div.button-container {
-		flex-direction: column;
-		padding: 0.5em;
-	}
-	button {
-		padding: 0.5em;
-	}
+    div {
+        display: flex;
+        background-color: black;
+        color: white;
+    }
+    div.button-container {
+        flex-direction: column;
+        padding: 0.5em;
+    }
+    button {
+        padding: 0.5em;
+    }
 </style>
