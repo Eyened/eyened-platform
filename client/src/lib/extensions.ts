062b614a056e5f60c56da9904cf479d86401924c
bjliefers <30621059+bjliefers@users.noreply.github.com>
2025-05-12 20:37:13 +0200

    Update and rename extensions.sample.ts to extensions.ts

---
+++ client/src/lib/extensions.ts
@@ -0,0 +1,22 @@
+const config = {
+    browser: {
+        patient: {
+            additional_data_sources: [
+            ]
+        },
+        study: {
+            list_forms: [
+            ],
+            additional_data_sources: [
+            ]
+        }
+    },
+    viewer: {
+        panel_info: {
+            additional_data_sources: [
+            ]
+        }
+    }
+};
+
+export default config; 
