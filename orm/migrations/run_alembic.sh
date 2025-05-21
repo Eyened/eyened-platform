99260e658e39fef93195577c4adf189d432ef447
Jeroen <jpvermeulen@hotmail.com>
2025-05-21 10:42:23 +0200

    optional sql check for migration

--- orm/migrations/run_alembic.sh
+++ orm/migrations/run_alembic.sh
@@ -6,21 +6,28 @@ eorm set_connection_string -e test
 echo "Creating tables and inserting test_data..."
 eorm test -s source -t test -r root_conditions.json
 
-echo "Running migration and output to migration.sql..."
+echo "Running migration and output to SQL..."
+
+# Define output path
+output_dir="sql"
+mkdir -p "$output_dir"  # Ensure directory exists
+
 # Get the current revision from the database
 current_rev=$(alembic current | grep -oE '[a-f0-9]{12}')
+
 # Get the latest head revision
 head_rev=$(alembic heads | grep -oE '[a-f0-9]{12}')
 
 # Generate SQL for migrations between the current revision and head
 if [ "$current_rev" != "$head_rev" ]; then
-    alembic upgrade $current_rev:$head_rev --sql > /app/migration_output/migration_latest.sql
-    echo "SQL for the latest migration generated: migration_output/migration_latest.sql"
+    alembic upgrade "$current_rev:$head_rev" --sql > "$output_dir/migration_latest.sql"
+    echo "SQL for the latest migration generated: $output_dir/migration_latest.sql"
 else
     echo "No new migrations to apply."
 fi
-alembic upgrade head
 
+# Actually apply the migration
+alembic upgrade head
 
 # Keep container running for debugging
 tail -f /dev/null
