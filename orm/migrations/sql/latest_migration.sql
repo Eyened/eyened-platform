1bf3b48c2be56f0af334a0d75b0a0167ec81c790
Jeroen <jpvermeulen@hotmail.com>
2025-10-22 11:09:18 +0200

    studyinstanceuid to series

--- orm/migrations/sql/latest_migration.sql
+++ orm/migrations/sql/latest_migration.sql
@@ -1,6 +1,18 @@
--- Running upgrade f198802eada6 -> 606596c6f965
+-- Running upgrade 62b55e9c9e70 -> 5ffd55a02beb
 
 DROP INDEX `ix_FormAnnotation_FormAnnotationReferenceID` ON `FormAnnotation`;
 
-UPDATE alembic_version SET version_num='606596c6f965' WHERE alembic_version.version_num = 'f198802eada6';
+ALTER TABLE `ImageInstance` ADD COLUMN `AltDatasetIdentifier` VARCHAR(256);
+
+ALTER TABLE `ImageInstance` MODIFY `Laterality` ENUM('L','R') NOT NULL;
+
+ALTER TABLE `Series` ADD COLUMN `StudyInstanceUid` VARCHAR(64);
+
+CREATE UNIQUE INDEX `StudyInstanceUidSeriesInstanceUid_UNIQUE` ON `Series` (`StudyInstanceUid`, `SeriesInstanceUid`);
+
+DROP INDEX `StudyInstanceUid_UNIQUE` ON `Study`;
+
+ALTER TABLE `Study` DROP COLUMN `StudyInstanceUid`;
+
+UPDATE alembic_version SET version_num='5ffd55a02beb' WHERE alembic_version.version_num = '62b55e9c9e70';
 
