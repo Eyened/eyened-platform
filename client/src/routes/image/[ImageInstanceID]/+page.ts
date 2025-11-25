74c2fb5ace45bc53abe9b42362b929f91f8fca36
Jose <j.vargasquiros@erasmusmc.nl>
2025-11-25 19:02:33 +0100

    removed bugged image route

--- client/src/routes/image/[ImageInstanceID]/+page.ts
+++
@@ -1,7 +0,0 @@
-export const ssr = false;
-
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch, params }) {
-    const ImageInstanceID = parseInt(params.ImageInstanceID);
-    return { ImageInstanceID };
-}
