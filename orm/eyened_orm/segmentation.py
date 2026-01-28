0323590961feb93ed0e249b85c7d1326cc0982cf
Karin van Garderen <k.vangarderen@erasmusmc.nl>
2026-01-28 12:24:41 +0100

    fix for segmentations with sparse axis

--- orm/eyened_orm/segmentation.py
+++ orm/eyened_orm/segmentation.py
@@ -302,15 +302,15 @@ class SegmentationBase(Base):
         # 2. Check Dimensions
         if self.ImageProjectionMatrix is None:
             # Strict match
-            if self.Depth != img_d:
+            if self.Depth != img_d and self.SparseAxis is not 0:
                 raise ValueError(
                     f"Depth mismatch: Segmentation {self.Depth} != Image {img_d}"
                 )
-            if self.Height != img_h:
+            if self.Height != img_h and self.SparseAxis is not 1:
                 raise ValueError(
                     f"Height mismatch: Segmentation {self.Height} != Image {img_h}"
                 )
-            if self.Width != img_w:
+            if self.Width != img_w and self.SparseAxis is not 2:
                 raise ValueError(
                     f"Width mismatch: Segmentation {self.Width} != Image {img_w}"
                 )
